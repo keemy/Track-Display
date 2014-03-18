d81a523ba54796e29705f0f78181a3e65fab0a54
root <root@plslamp.(none)>
2014-03-18 04:39:05 +0000

    bash script for automatically pulling database from web

---
+++ meets/something.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+TIME_OUT=$1
+FILE_URL=$2
+MEET_NAME=$3
+
+
+screen -t hello
+
+mkdir /var/www/Track-Display/meets/${MEET_NAME}
+cd /var/www/Track-Display/meets/${MEET_NAME}
+
+
+
+END_TIME=`date -d +${TIME_OUT}hours +%s`
+while [ `date +%s` -lt $END_TIME ]
+do
+wget ${FILE_URL} -O meet.mdb
+python /var/www/Track-Display/meets/mdbToSql.py meet.mdb | sqlite3 temp.db
+mv temp.db meet.db
+
+sleep 30
+done
