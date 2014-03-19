b08558d72f2d478ee22fdaf14243a253fda090ae
keemy <devin1891@gmail.com>
2014-03-19 12:48:17 -0700

    working on the ajax calls

--- getPeople.py
+++ getPeople.py
@@ -14,7 +14,7 @@ import os
 import sys
 cgitb.enable()
 
-meets=[i[1:] for i in os.listdir("/var/www/Track-Display/meets") where i[0]=="."]
+meets=[i[1:] for i in os.listdir("/var/www/Track-Display/meets") where i[0]=="#"]
 
 
 
--- js/VRCT.js
+++ js/VRCT.js
@@ -14,20 +14,48 @@ function getResults(race){
 		
 }
 
-var meetList;
+
 function getMeets(){
 	$.ajax("getMeets.py").done(function(data){
+			// to stuff to show meets 
+			data=$.parseJSON(data);
+			for(var i=0; i<data.length;i++){
+				var meet=data[i];
+				var option= "<option value='"+meet+"'>"+meet+"</option>"
+				$(".form #Meet").append(option)
+			}
+		})
+
+
+}
+
+
+function getEvents(){
+	$.ajax({
+		type: "GET",
+		data: {event: race},
+		url: "eventManager.py"
+		}).done(function(data){
 			// add logic to display 
-			
+		
 		
 		})
 
 
 }
 
-function getEvents(){}
+function getPeople(){
+	$.ajax({
+		type: "GET",
+		data: {event: race},
+		url: "eventManager.py"
+		}).done(function(data){
+			// add logic to display 
+		
+		
+		})
 
-function getPeople(){}
+}
 
 
 
