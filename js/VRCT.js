d1302d09d924686685f6f36c0da905bae16b4af2
keemy <devin1891@gmail.com>
2014-03-20 23:49:54 -0700

    added getting results for individuals

--- js/VRCT.js
+++ js/VRCT.js
@@ -1,20 +1,4 @@
 
-
-function getResults(race){
-//gets the results of a paricular event
-	$.ajax({
-		type: "GET",
-		data: {event: race},
-		url: "eventManager.py"
-		}).done(function(data){
-			// add logic to display 
-		
-		
-		})
-		
-}
-
-
 function getMeets(){
 	var promise = $.ajax("getMeets.py").done(function(data){
 		// to stuff to show meets 
@@ -24,7 +8,7 @@ function getMeets(){
 			var option= "<option value='"+meet+"'>"+meet+"</option>";
 			$(".form#Meet").append(option);
 		}
-	})
+	});
 	return promise
 }
 
@@ -67,7 +51,7 @@ function getEvents(meet){
 			}
 		
 		
-		})
+		});
 	return promise;
 
 }
@@ -86,11 +70,49 @@ function getPeople(meet){
 				$(".form#People").append(option);
 			}
 		
-		})
+		});
 	return promise;
 }
 
-
+function getResults(meet,eop,datum){
+//gets the results of a particular event/person
+	$.ajax({
+		type: "GET",
+		url: "getResults.py",
+		data:{
+			meetName:meet,
+			eventOrPerson:eop,
+			event:datum,
+			person:datum
+		}
+		}).done(function(data){
+			data=$.parseJSON(data);
+			
+			if(eop=="p"){
+				for(var i=0; i<data.length;i++){
+					var entry=data[i];
+					// entry = Fin_place, First_name, Last_name, Schl_yr, Team_name, Fin_time, 
+					//        Fin_wind, Fin_heat, Event_dist, Event_stroke, event_note, Event_sex
+					
+					var place="<td>"+entry[0]+"</td>";
+					var name="<td>"+entry[1]+" "+entry[2]+"</td>";
+					var year="<td>"+entry[3]+"</td>";
+					var team="<td>"+entry[4]+"</td>";
+					var time="<td>"+entry[5]+"</td>";
+					
+					
+					
+					
+					var out= "<tr>"+place+name+year+team+time+"</tr>";
+					
+					$("#dataNow").append(out)
+				}
+			}		
+				
+		
+		});
+		
+}
 
 
 
